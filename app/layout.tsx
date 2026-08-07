dafb39eb57eec53a4675d6f17bf5081fd10764cd
1arley <arthuriarley323@gmail.com>
2026-08-07 19:37:28 -0300

    chore: desativar push notifications e vignette banner (manter apenas onclick)

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,12 +42,13 @@ export default function RootLayout({
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${ADSENSE_CLIENT}`}
           crossOrigin="anonymous"
         />
-        <script
+        {/* Push Notifications (zona 11519348) - desativado temporariamente */}
+        {/* <script
           src="https://quge5.com/88/tag.min.js"
           data-zone="267619"
           async
           data-cfasync="false"
-        />
+        /> */}
       </head>
       <body
         className={`${fontInter.variable} ${fontSpaceGrotesk.variable} antialiased`}
